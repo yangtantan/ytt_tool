cadbc38ab1bac63c3a4ca00def865540916b131a
yangtantan <15895962685@163.com>
2016-09-18 18:18:55 +0800

    提交tool_common的pom文件

---
+++ untitled/src/main/java/ytt/tool/common/Demo1.java
@@ -0,0 +1,14 @@
+package ytt.tool.common;
+
+/**
+ * Created by Administrator on 2016/9/18.
+ */
+public class Demo1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+
+}
